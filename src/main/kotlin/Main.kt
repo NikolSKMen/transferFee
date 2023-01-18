7e87e240c3658f410d1f7ccc43e1d66ee34ea6ac
NikolSKMen <fuug@yandex.ru>
2023-01-18 19:08:14 +0300

    update pay

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,37 @@
 fun main() {
-    val itemTransfer =5000//перевод в рублях
+    var cardType="vk pay"//(Master card,Maestro) (Visa,Мир) (vk pay)
+    var transferSummMonth=0
+    val itemTransfer =14000//перевод в рублях
     val minCommission =35//миним. комисия
     val percentageCommission=0.75// процент комисии
     val totalPercent =percentageCommission/100 // итоговый размер комисии
+    val limTransferDay=150000
+    val limTransferMonth=600000
+    val limVkOnce=15000
+    val limVkMonth=40000
+    val limMasterMaestro=75000
 
-    var amount =if(itemTransfer * totalPercent > minCommission)itemTransfer*totalPercent+itemTransfer else itemTransfer +minCommission
-    println(amount)
+    var totalPrice=when(cardType){
+        "vk pay"->if(itemTransfer<=limVkOnce && transferSummMonth<=limVkMonth && itemTransfer<=limVkMonth){
+            itemTransfer
+        }else println("Превышен лимит")
+        "Master card","Maestro"->if(transferSummMonth<limMasterMaestro && itemTransfer<=limMasterMaestro ){
+            itemTransfer
+        }else if(itemTransfer<=limTransferDay && transferSummMonth<limTransferMonth && itemTransfer<=limTransferDay) {
+            itemTransfer*0.6/100+20+itemTransfer
+        }else println("Превышен лимит")
+
+        "Visa","Мир" ->if(itemTransfer * totalPercent > minCommission && transferSummMonth<limTransferMonth && itemTransfer<=limTransferDay){
+            itemTransfer*totalPercent+itemTransfer
+        }
+        else if (itemTransfer * totalPercent < minCommission){
+            itemTransfer +minCommission
+        }else println("Превышен лимит")
+        else->0
+    }
+
+
+if (totalPrice==0) println("нет данного способа оплаты")else
+    println("сумма перевода с карты $cardType c учетом комиссии составила $totalPrice руб.")
 
 }
